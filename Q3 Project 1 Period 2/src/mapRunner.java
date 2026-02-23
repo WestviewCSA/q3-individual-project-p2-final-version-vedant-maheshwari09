b355a2e11fcdd5243796fdd310c956451f598a2d
1966732 <1966732@01-18120.pusd.dom>
2026-02-23 12:16:53 -0800

    Started and setup mapRunner.java, and methods

---
+++ Q3 Project 1 Period 2/src/mapRunner.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class mapRunner {
+    
+    private static char[][][] mazes;
+    private static int numRows;
+    private static int numCols;
+    private static int numMazes;
+
+    public static void main(String[] args) {
+        System.out.println("Testing Text-Based Input");
+        readTextMap("EasyText1"); 
+        printMazes();
+
+        System.out.println("Testing Coordinate-Based Input");
+        readCoordinateMap("CoordinateText"); 
+        printMazes();
+    }
+
+  
+    public static void readTextMap(String filename) {
+        
+                }
+           
+           
+
+ 
+    public static void readCoordinateMap(String filename) {
+        
+    
+    }
+
+    public static void printMazes() {
+
+    }
+}
