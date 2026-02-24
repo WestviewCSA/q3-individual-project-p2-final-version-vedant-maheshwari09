27477e0a3bb75a68f4823686a18c0a951bf80a9e
1966732 <1966732@01-18120.pusd.dom>
2026-02-24 12:16:15 -0800

    Fixed method names and main (misunderstood assingment originally)

--- Q3 Project 1 Period 2/src/mapRunner.java
+++ Q3 Project 1 Period 2/src/mapRunner.java
@@ -3,36 +3,36 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class mapRunner {
-    
-    private static char[][][] mazes;
-    private static int numRows;
-    private static int numCols;
-    private static int numMazes;
 
-    public static void main(String[] args) {
-        System.out.println("Testing Text-Based Input");
-        readTextMap("EasyText1"); 
-        printMazes();
-
-        System.out.println("Testing Coordinate-Based Input");
-        readCoordinateMap("CoordinateText"); 
-        printMazes();
-    }
-
+    public static void main(String[] args) throws FileNotFoundException {
+    	// Here is the test Text-Based Map
+        String[][][] gridMap= getTextBasedMap("src/Easy.txt");
+        System.out.println("Text-Based Map:");
+        printMap(gridMap);
+        
+        // And here is the test Cordinate-Based Map
+        String[][][] coordMap= getCoordinateBasedMap("src/coordinate.txt");
+        System.out.println("\n");
+        System.out.println("Coordinate-Based Map:");
+        printMap(coordMap);
+        }
+        
   
-    public static void readTextMap(String filename) {
+    public static String[][][] getTextBasedMap(String fileName) throws FileNotFoundException {
         
                 }
            
            
 
  
-    public static void readCoordinateMap(String filename) {
+    public static String[][][] getCoordinateBasedMap(String fileName) throws FileNotFoundException {
+
         
     
     }
 
-    public static void printMazes() {
+    public static void printMap(String[][][] map) {
+
 
     }
 }
