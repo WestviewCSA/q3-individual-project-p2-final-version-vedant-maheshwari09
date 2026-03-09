c0368587fcc0003aba3cc3a35164219cf4f15b84
1966732 <1966732@01-18120.pusd.dom>
2026-03-09 11:20:22 -0700

    Location Helper Class (forgot to commit)

---
+++ Q3 Project 1 Period 2/src/Location.java
@@ -0,0 +1,23 @@
+
+public class Location {
+
+	public int row;
+    public int col;
+    public int level;
+
+    // This Location Constructor sets up the location
+    public Location(int r, int c, int l) {
+        this.row = r;
+        this.col = c;
+        this.level = l;
+    }
+
+    //printing corrdinates
+    @Override
+    public String toString() {
+        return "("+ row + ", "+ col + ", " + level+ ")";
+    }
+
+}
+
+
